0b78ff6c5ac97daf3395ff70191c38f60fb89c63
raja497 <83097824+raja497@users.noreply.github.com>
2021-09-03 11:57:36 +0530

    Update player.py

--- bot/safone/player.py
+++ bot/safone/player.py
@@ -160,12 +160,12 @@ allcmd = ["start", "help", f"start@{USERNAME}", f"help@{USERNAME}"] + admincmds
 async def not_chat(_, m: Message):
     buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/ABOUT_ABHINAS"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/DOSTI_GROUP_1234"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/VideoPlayerBot"),
+                InlineKeyboardButton("😘 OWNER 😘", url="https://t.me/abhinasroy"),
             ]
          ]
-    await m.reply_text(text="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [Source Code](https://github.com/AsmSafone/VideoPlayerBot) 😉!**", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await m.reply_text(text="**Sorry, You Can't Use This Bot In This Group! 🤷‍♂️ But You Can Make Your Own Bot Like This From The [OWNER](https://t.me/abhinasroy) 😉!**", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
